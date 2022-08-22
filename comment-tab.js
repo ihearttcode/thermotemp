8f3a6076e0d6a3d58bdf1c32432fb577d3987401
Ryan Lord <41973803+ihearttcode@users.noreply.github.com>
2022-08-22 12:33:50 -0500

    Update comment-tab.js

--- comment-tab.js
+++ comment-tab.js
@@ -1,2 +1,33 @@
+var document = window.document;
 
-window.document.querySelector("li[data-type=comment]").click();
+// Anonymous "self-invoking" function
+(function() {
+    var startingTime = new Date().getTime();
+    // Load the script
+    var script = document.createElement("SCRIPT");
+    script.src = 'https://code.jquery.com/jquery-3.6.0.min.js';
+    script.type = 'text/javascript';
+    document.getElementsByTagName("head")[0].appendChild(script);
+
+    // Poll for jQuery to come into existance
+    var checkReady = function(callback) {
+        if (window.jQuery) {
+            callback(jQuery);
+        }
+        else {
+            window.setTimeout(function() { checkReady(callback); }, 20);
+        }
+    };
+
+    // Start polling...
+    checkReady(function($) {
+        $(function() {
+            var endingTime = new Date().getTime();
+            var tookTime = endingTime - startingTime;
+            window.alert("jQuery is loaded, after " + tookTime + " milliseconds!");
+        });
+    });
+})();
+
+
+$("li[data-type=comment]").click();
