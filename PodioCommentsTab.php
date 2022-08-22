3be8036c43b946f6d49c09bae95e36dc07359abe
Ryan Lord <41973803+ihearttcode@users.noreply.github.com>
2022-08-22 16:03:21 -0500

    Update PodioCommentsTab.php

--- PodioCommentsTab.php
+++ PodioCommentsTab.php
@@ -1,5 +1,5 @@
 <html>
 
-<script type="text/javascript" async="" src="https://cdn.jsdelivr.net/gh/ihearttcode/thermotemp@main/podio-ticket-comment-tab.js"></script>
+<script type="text/javascript" async="" src="https://cdn.jsdelivr.net/gh/ihearttcode/thermotemp@main/podio-ticket-comments-tab.js"></script>
 
 </html>
