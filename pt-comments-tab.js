05aeb114bd68c8a2dcc62db2185eef0e5f06e975
Ryan Lord <41973803+ihearttcode@users.noreply.github.com>
2022-08-22 14:15:25 -0500

    Create pt-comments-tab.js

---
+++ pt-comments-tab.js
@@ -0,0 +1,34 @@
+var document = window.document;
+
+// Anonymous "self-invoking" function
+(function() {
+    var startingTime = new Date().getTime();
+    // Load the script
+    var script = document.createElement("SCRIPT");
+    script.src = 'https://code.jquery.com/jquery-3.6.0.min.js';
+    script.type = 'text/javascript';
+    document.getElementsByTagName("head")[0].appendChild(script);
+
+    // Poll for jQuery to come into existance
+    var checkReady = function(callback) {
+        if (window.jQuery) {
+            callback(jQuery);
+        }
+        else {
+            window.setTimeout(function() { checkReady(callback); }, 20);
+        }
+    };
+
+    // Start polling...
+    checkReady(function($) {
+        $(function() {
+            var endingTime = new Date().getTime();
+            var tookTime = endingTime - startingTime;
+            console.log("jQuery is loaded, after " + tookTime + " milliseconds!");
+        });
+    });
+})();
+
+if ( $("li[data-type=comment]").hasClass("clicked") === false ) {
+    $("li[data-type=comment]").addClass("clicked").click();
+};
