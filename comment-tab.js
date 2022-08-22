98765f2cc76080498d6ddf03899bb2004e851435
Ryan Lord <41973803+ihearttcode@users.noreply.github.com>
2022-08-22 10:08:52 -0500

    Update comment-tab.js

--- comment-tab.js
+++ comment-tab.js
@@ -1,8 +1,4 @@
 // Switches to the comments tab for items in app TTI Support on Podio
 
-document = window.document;
-
-comment = {
-    document.querySelector("li[data-type=comment]").click();
-};
+document.querySelector("li[data-type=comment]").click();
  
