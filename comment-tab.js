ed2531e6e343ba43e4a687957da7992a9cc2657f
Ryan Lord <41973803+ihearttcode@users.noreply.github.com>
2022-08-22 10:04:46 -0500

    Update and rename comment-tab.html to comment-tab.js

---
+++ comment-tab.js
@@ -0,0 +1,8 @@
+// Switches to the comments tab for items in app TTI Support on Podio
+
+document = window.document;
+
+comment = {
+    document.querySelector("li[data-type=comment]").click();
+};
+ 
