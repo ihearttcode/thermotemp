efb92be5bdb693b16d31fab5e58ed7475c8d6365
Ryan Lord <41973803+ihearttcode@users.noreply.github.com>
2022-08-24 11:42:29 -0500

    Create comment-v5.js

---
+++ comment-v5.js
@@ -0,0 +1,13 @@
+var document = window.document;
+
+var commentTab = function() {
+      if (document.querySelector("ol li[data-type=comment]").classList.contains("clicked") === false) {
+        var com = document.querySelector("ol li[data-type=comment]");
+        var activity = document.querySelector("ol li[data-type=activity]");
+        com.click();
+        com.classList.add("active","clicked");
+        com.style.backgroundColor="yellow";
+        activity.classList.remove("active");
+        };
+    };
+document.body.addEventListener("load", commentTab());
