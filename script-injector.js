48708d2ca42473e560a450483434795e22f9f291
Ryan Lord <41973803+ihearttcode@users.noreply.github.com>
2022-08-24 12:57:31 -0500

    Delete script-injector.js

--- script-injector.js
+++
@@ -1,5 +0,0 @@
-var script = document.createElement("SCRIPT");
-//script.async = '';
-script.src = 'https://cdn.jsdelivr.net/gh/ihearttcode/thermotemp@main/comment-v4.js';
-script.type = 'text/javascript';
-document.body.appendChild(script);
