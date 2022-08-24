5074981b4b241c38568690e5db8cdae6fdfe029c
Ryan Lord <41973803+ihearttcode@users.noreply.github.com>
2022-08-24 08:26:45 -0500

    Create script-injector.js

---
+++ script-injector.js
@@ -0,0 +1,4 @@
+var script = document.createElement("SCRIPT");
+script.src = 'https://cdn.jsdelivr.net/gh/ihearttcode/thermotemp@main/comment-v4.js';
+script.type = 'text/javascript';
+document.getElementsByTagName("head")[0].appendChild(script);
