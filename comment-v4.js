7971231312e2d8df89f741db25faa0c36ccbd780
Ryan Lord <41973803+ihearttcode@users.noreply.github.com>
2022-08-23 23:17:56 -0500

    Update comment-v4.js

--- comment-v4.js
+++ comment-v4.js
@@ -1,15 +1,13 @@
 var document = window.document;
 
-(function() {
-
-  var checkReady = checkReady(); function checkReady() {
-      if (document.querySelector("ol li[data-type=comment]").classList.contains("active") === false) {
+var commentTab = function() {
+      if (document.querySelector("ol li[data-type=comment]").classList.contains("clicked") === false) {
         var com = document.querySelector("ol li[data-type=comment]");
         var activity = document.querySelector("ol li[data-type=activity]");
         com.click();
-        com.classList.add("active");
+        com.classList.add("active","clicked");
         com.style.backgroundColor="yellow";
         activity.classList.remove("active");
-        };
+        }
     };
-});
+commentTab(document);
