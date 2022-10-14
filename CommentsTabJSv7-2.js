7ea201afbbd21030fb0968fb00098003d833e1ad
Ryan Lord <41973803+ihearttcode@users.noreply.github.com>
2022-10-14 13:39:33 -0500

    Create CommentsTabJSv7-2.js

---
+++ CommentsTabJSv7-2.js
@@ -0,0 +1,14 @@
+function resolveAfter5Seconds(x,y) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(x);
+      resolve(y);
+    }, 5000);
+  });
+}
+async function clickCommentsTab() {
+  const x = await resolveAfter5Seconds('Comment Tab Clicked');
+  console.log(x); // 'Comment Tab clicked'
+  const y = await resolveAfter5Seconds({document.querySelector('li[data-type=comment]').click()});
+}
+clickCommentsTab()
