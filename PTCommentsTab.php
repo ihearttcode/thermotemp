1594294508fef8a18a14e2b0b0022fd3c8cae48e
Ryan Lord <41973803+ihearttcode@users.noreply.github.com>
2022-10-14 11:42:06 -0500

    Update PTCommentsTab.php

--- PTCommentsTab.php
+++ PTCommentsTab.php
@@ -1,5 +1,5 @@
 <html>
 
-<script type="text/javascript" async="true" src="https://cdn.jsdelivr.net/gh/ihearttcode/thermotemp@main/CommentsTabJS.js"></script>
+<script type="text/javascript" async="true" src="https://cdn.jsdelivr.net/gh/ihearttcode/thermotemp@main/CommentsTabJSv2.js"></script>
 
 </html>
