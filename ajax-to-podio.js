2675222bbd6af1766d4415c2d59927bac0f1ee95
Ryan Lord <41973803+ihearttcode@users.noreply.github.com>
2023-03-13 10:34:23 -0500

    Create ajax-to-podio.js

---
+++ ajax-to-podio.js
@@ -0,0 +1,28 @@
+var postCampaign = {
+  "url": "https://workflow-automation.podio.com/catch/g06gh8cy8mv73kd",
+  "method": "POST",
+  "timeout": 0,
+  "headers": {
+    "Content-Type": "application/x-www-form-urlencoded"
+  },
+  "data": {
+    "emailAddress": getQueryVariable("emailAddress"),
+    "firstName": getQueryVariable("firstName"),
+    "lastName": getQueryVariable("lastName"),
+    "companyName": getQueryVariable("companyName"),
+    "jobTitle": getQueryVariable("jobTitle")
+  }
+};
+
+
+function ajaxToPodio(postCampaign) {
+    $.ajax(postCampaign).done(function () {
+        console.log("Ajax Completed");
+    }
+        );
+    }
+function processAjaxToPodio(callback) {
+    callback(postCampaign);
+}
+
+processAjaxToPodio(ajaxToPodio);
