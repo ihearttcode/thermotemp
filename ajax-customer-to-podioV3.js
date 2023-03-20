55e63e548be0979978b5703a304d6ccd54c7de96
Ryan Lord <41973803+ihearttcode@users.noreply.github.com>
2023-03-20 14:30:06 -0500

    Create ajax-customer-to-podioV3.js

---
+++ ajax-customer-to-podioV3.js
@@ -0,0 +1,43 @@
+function resolveAfter2Seconds() {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve('resolved');
+    }, 2000);
+  });
+}
+
+async function asyncCall() {
+  console.log('calling');
+  const result = await resolveAfter2Seconds();
+  console.log(result);
+  var postCampaign = {
+    "url": "https://workflow-automation.podio.com/catch/g06gh8cy8mv73kd",
+    "method": "POST",
+    "timeout": 0,
+    "headers": {
+      "Content-Type": "application/x-www-form-urlencoded"
+    },
+    "data": {
+      "emailAddress": getQueryVariable("emailAddress"),
+      "firstName": getQueryVariable("firstName"),
+      "lastName": getQueryVariable("lastName"),
+      "companyName": getQueryVariable("companyName"),
+      "jobTitle": getQueryVariable("jobTitle"),
+      "campaign": getQueryVariable("campaign")
+    }
+  };
+  
+  
+  function ajaxToPodio(customer) {
+    jQuery.ajax(customer).done(function() {
+      console.log("Ajax Completed");
+    });
+  }
+
+  if (postCampaign.data.emailAddress.toString() != "false") {
+
+  ajaxToPodio(postCampaign);
+  }
+}
+
+asyncCall();
