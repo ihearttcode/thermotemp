65df57fc9774ec8af69e8e950ce01917aebbbb0b
Ryan Lord <41973803+ihearttcode@users.noreply.github.com>
2023-03-30 16:12:13 -0500

    Create commentTabScript2.js

---
+++ commentTabScript2.js
@@ -0,0 +1,12 @@
+var document = window.document;
+function clickCommentTab() {
+  var commentsTab = document.querySelector("li[data-type='comment']");
+  if (!commentsTab.classList.contains("clicked")) {
+    var activity = document.querySelector("li[data-type='activity']");
+    commentsTab.click();
+    commentsTab.classList.add("active", "clicked");
+    commentsTab.style.backgroundColor = "yellow";
+    activity.classList.remove("active");
+  }
+}
+clickCommentTab();
